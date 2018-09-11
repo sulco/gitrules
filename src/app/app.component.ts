950ee970eba13938d1da2886082e36ef3b0a5bd4
Tomek Sulkowski <tomek.sulkowski@gmail.com>
2018-09-11 19:40:43 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gitrules';
+}
